08fd7dadc301ef02c5ab777bfe9f75880b11224f
Suliman <sadakhanov_s@dogma.ru>
2024-04-20 21:30:55 +0300

    Сверстана страница main

--- hackathon-3-front/src/main.tsx
+++ hackathon-3-front/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ConfigProvider } from 'gentlemen-ui-kit';
+import './index.css';
 
 const queryClient = new QueryClient();
 
